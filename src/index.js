2d51faa149bf8e9e3255028e163dbeffd95c89d1
Ivan <me@example.com>
2019-02-10 20:52:37 -0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ClassNames from './class_names';
+import ClassesNesting from './classes_nesting';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Mui extends React.Component {
+  render() {
+    return (
+      <div>
+        <ClassNames />
+        <ClassesNesting />
+      </div>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Mui />, document.getElementById('root'));
