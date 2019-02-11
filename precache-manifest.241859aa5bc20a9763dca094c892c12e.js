1714f045b07082199fa56ee93421126fd74b3893
Ivan <me@example.com>
2019-02-10 20:58:59 -0600

    Updates

---
+++ precache-manifest.241859aa5bc20a9763dca094c892c12e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "25951d94941648ed7e4a",
+    "url": "/effective-giggle/static/js/main.25951d94.chunk.js"
+  },
+  {
+    "revision": "f75f9fc7328d3a1ae631",
+    "url": "/effective-giggle/static/js/runtime~main.f75f9fc7.js"
+  },
+  {
+    "revision": "3cfb5c4dfe2231b829d2",
+    "url": "/effective-giggle/static/js/2.3cfb5c4d.chunk.js"
+  },
+  {
+    "revision": "21c26b3d8dfb0f83b6868d72a09e2ed8",
+    "url": "/effective-giggle/index.html"
+  }
+];
